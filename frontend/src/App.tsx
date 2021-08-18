abb04e372d3e91060bea823da052611327308139
Jack Nugent <31449177+jacknugent@users.noreply.github.com>
2021-08-18 19:25:25 -0400

    Introduce landing page (#15)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './HomePage/HomePage';
+import Layout from './Layout';
+
+export default function App() {
+    return (
+        <Layout>
+            <HomePage />
+        </Layout>
+    );
+}
