762de0b7e94648bd968634716aff0298d6b4a581
Jack Nugent <jacknugent27@gmail.com>
2021-08-18 18:52:51 -0400

    Add US territories to state list

--- frontend/src/HomePage/HomePage.tsx
+++ frontend/src/HomePage/HomePage.tsx
@@ -58,6 +58,7 @@ export default function HomePage() {
                             <option value="">Select</option>
                             <option value="AL">Alabama</option>
                             <option value="AK">Alaska</option>
+                            <option value="AS">American Samoa</option>
                             <option value="AZ">Arizona</option>
                             <option value="AR">Arkansas</option>
                             <option value="CA">California</option>
@@ -67,6 +68,7 @@ export default function HomePage() {
                             <option value="DC">District Of Columbia</option>
                             <option value="FL">Florida</option>
                             <option value="GA">Georgia</option>
+                            <option value="GU">Guam</option>
                             <option value="HI">Hawaii</option>
                             <option value="ID">Idaho</option>
                             <option value="IL">Illinois</option>
@@ -91,10 +93,12 @@ export default function HomePage() {
                             <option value="NY">New York</option>
                             <option value="NC">North Carolina</option>
                             <option value="ND">North Dakota</option>
+                            <option value="MP">Northern Mariana Islands</option>
                             <option value="OH">Ohio</option>
                             <option value="OK">Oklahoma</option>
                             <option value="OR">Oregon</option>
                             <option value="PA">Pennsylvania</option>
+                            <option value="PR">Puerto Rico</option>
                             <option value="RI">Rhode Island</option>
                             <option value="SC">South Carolina</option>
                             <option value="SD">South Dakota</option>
@@ -102,6 +106,7 @@ export default function HomePage() {
                             <option value="TX">Texas</option>
                             <option value="UT">Utah</option>
                             <option value="VT">Vermont</option>
+                            <option value="VI">Virgin Islands</option>
                             <option value="VA">Virginia</option>
                             <option value="WA">Washington</option>
                             <option value="WV">West Virginia</option>
