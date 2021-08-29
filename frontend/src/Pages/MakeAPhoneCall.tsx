89159ea86b2270893e2067ca957c27a60e23b38b
Jack Nugent <31449177+jacknugent@users.noreply.github.com>
2021-08-29 15:13:43 -0400

    Add height to each section (#49)

--- frontend/src/Pages/MakeAPhoneCall.tsx
+++ frontend/src/Pages/MakeAPhoneCall.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function MakeAPhoneCall({ isPhoneCallMade, setIsPhoneCallMade }: Props) {
     return (
         <div className="pt-2 pb-3">
-            <h3 className="text-start pb-3">Make a Phone Call</h3>
+            <h3 className="text-start pb-3 step-height">Make a Phone Call</h3>
             <Button className="d-flex me-auto" disabled={isPhoneCallMade} onClick={() => setIsPhoneCallMade(true)}>Done With Calls</Button>
         </div>
     )
--- frontend/src/Pages/PostOnSocial.tsx
+++ frontend/src/Pages/PostOnSocial.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function PostOnSocial({ isSocialPosted, setIsSocialPosted }: Props) {
     return (
         <div className="pt-2 pb-3">
-            <h3 className="text-start pb-3">Post on Social</h3>
+            <h3 className="text-start pb-3 step-height">Post on Social</h3>
             <Button className="d-flex me-auto" disabled={isSocialPosted} onClick={() => setIsSocialPosted(true)}>Tweet Now</Button>
         </div>
     )
