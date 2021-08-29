b82b03e60e6a63f80b3eab6904eb351bc413261f
Jack Nugent <jacknugent27@gmail.com>
2021-08-29 13:14:38 -0400

    Move style

--- frontend/src/Pages/PickYourIssue.tsx
+++ frontend/src/Pages/PickYourIssue.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 export default function PickYourIssue({ issue, setIssue }: Props) {
     return (
-        <div className="pt-2 pb-3 step-height">
-            <h2 className="text-start pb-3">Pick Your Issue</h2>
+        <div className="pt-2 pb-3">
+            <h2 className="text-start pb-3 step-height">Pick Your Issue</h2>
             <Button className="d-flex me-auto" onClick={() => setIssue("Issue XYZ")} disabled={!!issue}>Issue XYZ</Button>
         </div>
     )
--- frontend/src/Pages/SendAnEmail.tsx
+++ frontend/src/Pages/SendAnEmail.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 export default function SendAnEmail({ isEmailSent, setIsEmailSent }: Props) {
     return (
-        <div className="pt-2 pb-3 step-height">
-            <h3 className="text-start pb-3">Send An Email</h3>
+        <div className="pt-2 pb-3">
+            <h3 className="text-start pb-3 step-height">Send An Email</h3>
             <Button className="d-flex me-auto" disabled={isEmailSent} onClick={() => setIsEmailSent(true)}>Send Email</Button>
         </div>
     )
