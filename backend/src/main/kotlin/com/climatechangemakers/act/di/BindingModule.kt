b9c269bceeb1df25ca4c764545e79048e54e237b
Kevin Cianfarini <kevincianfarini@gmail.com>
2021-09-10 08:57:14 -0400

    Record legislator email action
    References #14, #56

--- backend/src/main/kotlin/com/climatechangemakers/act/di/BindingModule.kt
+++ backend/src/main/kotlin/com/climatechangemakers/act/di/BindingModule.kt
@@ -1,5 +1,7 @@
 package com.climatechangemakers.act.di
 
+import com.climatechangemakers.act.feature.action.manager.ActionTrackerManager
+import com.climatechangemakers.act.feature.action.manager.DatabaseActionTrackerManager
 import com.climatechangemakers.act.feature.issue.manager.DatabaseIssueManager
 import com.climatechangemakers.act.feature.issue.manager.IssueManager
 import com.climatechangemakers.act.feature.lcvscore.manager.DatabaseLcvScoreManager
@@ -11,4 +13,5 @@ import dagger.Module
 
   @Binds fun bindsLcvScoreManager(manager: DatabaseLcvScoreManager): LcvScoreManager
   @Binds fun bindsIssueManager(manager: DatabaseIssueManager): IssueManager
+  @Binds fun bindsActionTrackerManager(manager: DatabaseActionTrackerManager): ActionTrackerManager
 }
