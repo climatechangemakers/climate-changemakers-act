ec77f56ae0d5c463ea70f829db1c1e25b38401df
Kevin Cianfarini <kevincianfarini@gmail.com>
2021-09-19 20:07:36 -0400

    Handle empty legislator LCV score list (#71)
    Co-authored-by: Jack Nugent <jacknugent27@gmail.com>

--- frontend/src/models/ActionInfo.ts
+++ frontend/src/models/ActionInfo.ts
@@ -1,24 +1,26 @@
 export type ActionInfo = {
     initiatorEmail: string;
-    legislators: {
-        imageUrl: string;
-        area: {
-            state: string;
-            districtNumber?: number;
+    legislators: Legislator[];
+}
+
+export type Legislator = {
+    imageUrl: string;
+    area: {
+        state: string;
+        districtNumber?: number;
+    };
+    partyAffiliation: string;
+    lcvScores: {
+        score: number;
+        scoreType: {
+            type: "lifetime";
+        } | {
+            type: "year";
+            year: number;
         };
-        partyAffiliation: string;
-        lcvScores: {
-            score: number;
-            scoreType: {
-                type: "lifetime";
-            } | {
-                type: "year";
-                year: number;
-            };
-        }[];
-        name: string;
-        phoneNumbers: string[];
-        role: string;
-        siteUrl: string;
     }[];
+    name: string;
+    phoneNumbers: string[];
+    role: string;
+    siteUrl: string;
 }
