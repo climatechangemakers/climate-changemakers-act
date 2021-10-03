9901d137fce81a7135c8d965216aff14dfea414c
Kevin Cianfarini <kevincianfarini@gmail.com>
2021-10-03 14:03:48 -0400

    Implement initial logging policy
    Closes #27

--- backend/src/main/kotlin/org/climatechangemakers/act/plugins/ExceptionHandler.kt
+++ backend/src/main/kotlin/org/climatechangemakers/act/plugins/ExceptionHandler.kt
@@ -3,6 +3,7 @@ package org.climatechangemakers.act.plugins
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.application.log
 import io.ktor.features.StatusPages
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
@@ -14,15 +15,17 @@ fun Application.configureExceptionHandler() {
   install(StatusPages) {
 
     exception<SerializationException> { cause ->
+      cause.message?.let(log::error)
       call.respond(HttpStatusCode.BadRequest, cause.message ?: "")
     }
 
     exception<HttpException> { cause ->
-      // TODO(kcianfarini) log this
+      cause.message?.let(log::error)
       call.respond(HttpStatusCode.InternalServerError, cause.message())
     }
 
     exception<Exception> { cause ->
+      cause.message?.let(log::error)
       call.respond(HttpStatusCode.InternalServerError, cause.message ?: "")
     }
   }
