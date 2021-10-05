0670ee9e43379412eee3f3a29acbefe0327bfee4
Kevin Cianfarini <kevincianfarini@gmail.com>
2021-10-05 08:57:10 -0400

    Add necessary fields to `SendEmailRequest`
    This commit adds fields necessary to make a (S)CWC contact http call.
    Some of this information has already been collected from the user as
    part of finding their members of congress -- we should reuse that
    information versus redundantly asking for it again.
    
    The `relatedTopics` and `contactedBioguideIds` always expect to have at
    least one element in them.
    
    Closes #56

--- backend/src/main/kotlin/org/climatechangemakers/act/feature/action/model/SendEmailRequest.kt
+++ backend/src/main/kotlin/org/climatechangemakers/act/feature/action/model/SendEmailRequest.kt
@@ -1,10 +1,27 @@
 package org.climatechangemakers.act.feature.action.model
 
 import kotlinx.serialization.Serializable
+import org.climatechangemakers.act.common.model.RepresentedArea
+import org.climatechangemakers.act.feature.communicatewithcongress.model.Prefix
+import org.climatechangemakers.act.feature.communicatewithcongress.model.Topic
 
 @Serializable class SendEmailRequest(
   val originatingEmailAddress: String,
-  val relatedIssueId: Long,
+  val title: Prefix,
+  val firstName: String,
+  val lastName: String,
+  val streetAddress: String,
+  val city: String,
+  val state: RepresentedArea,
+  val postalCode: String,
+  val relatedTopics: List<Topic>,
+  val emailSubject: String,
   val emailBody: String,
+  val relatedIssueId: Long,
   val contactedBioguideIds: List<String>,
-)
+) {
+  init {
+    require(contactedBioguideIds.isNotEmpty())
+    require(relatedTopics.isNotEmpty())
+  }
+}
