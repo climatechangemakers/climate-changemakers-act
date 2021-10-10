9705a527f2713f86e8cebdc4f6e96e7628ac7a3a
Evan Hahn <me@evanhahn.com>
2021-10-10 14:28:42 -0500

    Move contents into `return`

--- frontend/src/Pages/TakeAction/PostOnSocial.tsx
+++ frontend/src/Pages/TakeAction/PostOnSocial.tsx
@@ -1,7 +1,6 @@
-import { ReactChild, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Button, Form, InputGroup } from "react-bootstrap";
 import type Loadable from "common/lib/Loadable";
-import MissingCaseError from "common/lib/MissingCaseError";
 import { isTweetValid, getPostTweetUrl } from "common/lib/twitter";
 
 type Props = {
@@ -26,98 +25,85 @@ export default function PostOnSocial({
         }
     }, [preComposedTweet]);
 
-    let contents: ReactChild;
-    switch (preComposedTweet.status) {
-        case "loading":
-            contents = <p>Loading...</p>;
-            break;
-        case "failed":
-            contents = <p>Failed to load. Please try refreshing the page.</p>;
-            break;
-        case "loaded":
-            contents = (
-                <Form
-                    onSubmit={(event) => {
-                        event.preventDefault();
-                        event.stopPropagation();
-
-                        if (hasClickedLink) {
-                            setIsSocialPosted(true);
-                        } else if (window.open(getPostTweetUrl(tweet.trim()))) {
-                            setHasClickedLink(true);
-                            logTweet();
-                        }
-                    }}
-                >
-                    <Form.Group>
-                        <Form.Label htmlFor="draft-tweet-input">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus!
-                        </Form.Label>
-                        <InputGroup hasValidation>
-                            <Form.Control
-                                as="textarea"
-                                rows={4}
-                                maxLength={1000}
-                                id="draft-tweet-input"
-                                placeholder="Compose your tweet"
-                                isInvalid={!isTweetValid(tweet)}
-                                disabled={isSocialPosted || hasClickedLink}
-                                value={tweet}
-                                onChange={(event) => {
-                                    setTweet(event.target.value);
-                                }}
-                            />
-                            <Form.Control.Feedback type="invalid" tooltip>
-                                {tweet.trim() ? "Your tweet is invalid. Is it too long?" : "You must enter a tweet."}
-                            </Form.Control.Feedback>
-                        </InputGroup>
-                    </Form.Group>
-                    <div className="row mt-3">
-                        <div className="col d-flex">
-                            <Button
-                                className="flex-grow-1 mr-2"
-                                variant="secondary"
-                                disabled={isSocialPosted}
-                                onClick={() => {
-                                    setIsSocialPosted(true);
-                                }}
-                            >
-                                Skip Tweeting
-                            </Button>
-                        </div>
-                        <div className="col d-flex">
-                            {hasClickedLink ? (
-                                <Button
-                                    type="submit"
-                                    className="flex-grow-1 ml-2"
-                                    variant="primary"
-                                    disabled={isSocialPosted}
-                                >
-                                    Done Tweeting
-                                </Button>
-                            ) : (
-                                <Button
-                                    type="submit"
-                                    className="flex-grow-1 ml-2"
-                                    variant="primary"
-                                    disabled={isSocialPosted || !isTweetValid(tweet)}
-                                >
-                                    Send Tweet
-                                </Button>
-                            )}
-                        </div>
-                    </div>
-                </Form>
-            );
-            break;
-        default:
-            throw new MissingCaseError(preComposedTweet);
-    }
-
     return (
         <div className="pt-2 pb-3">
             <h3 className="text-start pb-3">Post on Social</h3>
-            {contents}
+            {preComposedTweet.status === "loading" ? <p>Loading...</p>
+             : preComposedTweet.status === "failed" ? <p>Failed to load. Please try refreshing the page.</p>
+             : (
+                 <Form
+                     onSubmit={(event) => {
+                         event.preventDefault();
+                         event.stopPropagation();
+
+                         if (hasClickedLink) {
+                             setIsSocialPosted(true);
+                         } else if (window.open(getPostTweetUrl(tweet.trim()))) {
+                             setHasClickedLink(true);
+                             logTweet();
+                         }
+                     }}
+                 >
+                     <Form.Group>
+                         <Form.Label htmlFor="draft-tweet-input">
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus!
+                         </Form.Label>
+                         <InputGroup hasValidation>
+                             <Form.Control
+                                 as="textarea"
+                                 rows={4}
+                                 maxLength={1000}
+                                 id="draft-tweet-input"
+                                 placeholder="Compose your tweet"
+                                 isInvalid={!isTweetValid(tweet)}
+                                 disabled={isSocialPosted || hasClickedLink}
+                                 value={tweet}
+                                 onChange={(event) => {
+                                     setTweet(event.target.value);
+                                 }}
+                             />
+                             <Form.Control.Feedback type="invalid" tooltip>
+                                 {tweet.trim() ? "Your tweet is invalid. Is it too long?" : "You must enter a tweet."}
+                             </Form.Control.Feedback>
+                         </InputGroup>
+                     </Form.Group>
+                     <div className="row mt-3">
+                         <div className="col d-flex">
+                             <Button
+                                 className="flex-grow-1 mr-2"
+                                 variant="secondary"
+                                 disabled={isSocialPosted}
+                                 onClick={() => {
+                                     setIsSocialPosted(true);
+                                 }}
+                             >
+                                 Skip Tweeting
+                             </Button>
+                         </div>
+                         <div className="col d-flex">
+                             {hasClickedLink ? (
+                                 <Button
+                                     type="submit"
+                                     className="flex-grow-1 ml-2"
+                                     variant="primary"
+                                     disabled={isSocialPosted}
+                                 >
+                                     Done Tweeting
+                                 </Button>
+                             ) : (
+                                 <Button
+                                     type="submit"
+                                     className="flex-grow-1 ml-2"
+                                     variant="primary"
+                                     disabled={isSocialPosted || !isTweetValid(tweet)}
+                                 >
+                                     Send Tweet
+                                 </Button>
+                             )}
+                         </div>
+                     </div>
+                 </Form>
+            )}
         </div>
     )
 }
--- frontend/src/common/lib/MissingCaseError.ts
+++
@@ -1,5 +0,0 @@
-export default class MissingCaseError extends Error {
-    constructor(shouldNeverHappen: never) {
-        super(`Got an unexpected case: ${String(shouldNeverHappen)}`);
-    }
-}
