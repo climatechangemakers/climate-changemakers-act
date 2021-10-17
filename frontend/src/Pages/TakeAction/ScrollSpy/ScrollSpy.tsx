15a3d5ae9c22231a9105532e24090f4fa4e9ce1c
Jack Nugent <31449177+jacknugent@users.noreply.github.com>
2021-10-17 18:30:09 -0400

    Redesign scrollspy (#132)
    * Scrollspy WIP
    
    * Redesign scrollspy
    
    * Move files
    
    * Remove mobile from MobileScrollSpy name
    
    * Separate desktop from mobile
    
    * PR changes
    
    * Format changes
    
    * Remove duplicate code
    
    * Change scroll logic
    
    * Undo scroll change
    
    * Add scrollspy buffer
    
    * Move up const
    
    * add background

--- frontend/src/Pages/TakeAction/ScrollSpy/ScrollSpy.tsx
+++ frontend/src/Pages/TakeAction/ScrollSpy/ScrollSpy.tsx
@@ -1,31 +1,85 @@
-import { Container } from "react-bootstrap";
-import DesktopProgressBar from "./DesktopScrollSpy/DesktopScrollSpy";
-import MobileProgressBar from "./MobileScrollSpy/MobileScrollSpy";
-import styles from "./ScrollSpy.module.css";
+import cx from "classnames";
+import { useEffect, useState } from "react";
+import { Card, ProgressBar } from "react-bootstrap";
+import styles from "./MobileScrollSpy.module.css";
+import Step from "./Step";
 
 type Props = {
     isEmailSent: boolean;
     isPhoneCallMade: boolean;
     isSocialPosted: boolean;
+    desktop?: boolean;
 };
 
-export default function ScrollSpy({ isEmailSent, isPhoneCallMade, isSocialPosted }: Props) {
+const INTRO_SCROLL_BUFFER = 40;
+
+export default function ScrollSpy({ isEmailSent, isPhoneCallMade, isSocialPosted, desktop = false }: Props) {
+    const [introSectionDistanceFromTop, setIntroSectionDistanceFromTop] = useState(Math.min);
+    const [scrolledPastIntro, setScrolledPastIntro] = useState(false);
+
+    useEffect(() => {
+        if (introSectionDistanceFromTop - INTRO_SCROLL_BUFFER <= 0) setScrolledPastIntro(true)
+    }, [introSectionDistanceFromTop])
+
+    useEffect(() => {
+        const handleResize = () =>
+            setIntroSectionDistanceFromTop(document.getElementById("send_an_email")!.getBoundingClientRect()!.top);
+
+        handleResize();
+        window.addEventListener("scroll", handleResize);
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("scroll", handleResize);
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    const linkState = (isComplete: boolean, isActive: boolean) =>
+        isComplete ? "complete" : isActive ? "active" : "disabled";
+
     return (
-        <div className="flex-grow-1 d-flex justify-content-center">
-            <div className="position-fixed d-none d-lg-block">
-                <DesktopProgressBar
-                    isEmailSent={isEmailSent}
-                    isPhoneCallMade={isPhoneCallMade}
-                    isSocialPosted={isSocialPosted}
-                />
-            </div>
-            <div className={`d-block d-lg-none w-100 position-fixed ${styles.mobileProgress}`}>
-                <MobileProgressBar
-                    isEmailSent={isEmailSent}
-                    isPhoneCallMade={isPhoneCallMade}
-                    isSocialPosted={isSocialPosted}
-                />
-            </div>
-        </div>
+        <Card
+            className={cx({
+                "position-fixed border-0 mt-4": desktop,
+                "ms-3 me-3": !desktop,
+            })}
+        >
+            <Card.Body
+                className={cx("d-flex justify-content-between text-dark pt-3 pb-2 ps-0 pe-0", {
+                    "flex-column bg-dark-purple text-white": desktop,
+                    "bg-light-grey": !desktop
+                })}
+            >
+                {desktop && (
+                    <h3 className="mb-0">
+                        <span className="ms-4 ps-3" />
+                        Steps
+                    </h3>
+                )}
+                <Step step={1} id="#introduction" state={linkState(scrolledPastIntro, true)} desktop={desktop}>
+                    {desktop ? "Introduction" : "Intro"}
+                </Step>
+                <Step step={2} id="#send_an_email" state={linkState(isEmailSent, true)} desktop={desktop}>
+                    {desktop ? "Send an Email" : "Email"}
+                </Step>
+                <Step
+                    step={3}
+                    id="#make_a_phone_call"
+                    state={linkState(isPhoneCallMade, isEmailSent)}
+                    desktop={desktop}
+                >
+                    {desktop ? "Make a Call" : "Call"}
+                </Step>
+                <Step
+                    step={4}
+                    id="#post_on_social"
+                    state={linkState(isSocialPosted, isPhoneCallMade)}
+                    last
+                    desktop={desktop}
+                >
+                    {desktop ? "Post on Social" : "Post"}
+                </Step>
+            </Card.Body>
+        </Card>
     );
 }
