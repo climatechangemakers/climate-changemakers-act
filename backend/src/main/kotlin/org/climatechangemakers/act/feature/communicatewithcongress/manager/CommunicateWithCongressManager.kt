1913e8eb43b04a713b07ba50f0d392c3f76f679d
Kevin Cianfarini <kevincianfarini@gmail.com>
2021-10-19 21:48:35 -0400

    Respond with email body on successful request
    Closes #118

--- backend/src/main/kotlin/org/climatechangemakers/act/feature/communicatewithcongress/manager/CommunicateWithCongressManager.kt
+++ backend/src/main/kotlin/org/climatechangemakers/act/feature/communicatewithcongress/manager/CommunicateWithCongressManager.kt
@@ -1,12 +1,13 @@
 package org.climatechangemakers.act.feature.communicatewithcongress.manager
 
+import org.climatechangemakers.act.common.model.Result
 import org.climatechangemakers.act.feature.action.model.SendEmailRequest
 
 interface CommunicateWithCongressManager {
 
   /**
    * Send emails to multiple members of congress as specified in [SendEmailRequest.contactedBioguideIds].
-   * This function returns a list of bioguide IDs for which sending an email failed.
+   * This function returns either the email body that was sent, or a list of failed bioguide IDs.
    */
-  suspend fun sendEmails(request: SendEmailRequest): List<String>
+  suspend fun sendEmails(request: SendEmailRequest): Result<String, List<String>>
 }
