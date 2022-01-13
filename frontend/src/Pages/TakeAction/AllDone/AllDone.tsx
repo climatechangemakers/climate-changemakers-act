6d781a3a5dfd3d49b5ed1161eca06681efcb854e
Jack Nugent <31449177+jacknugent@users.noreply.github.com>
2022-01-12 23:36:12 -0500

    Nice work copy/style changes (#255)

--- frontend/src/Pages/TakeAction/AllDone/AllDone.tsx
+++ frontend/src/Pages/TakeAction/AllDone/AllDone.tsx
@@ -33,22 +33,18 @@ export default function AllDone() {
             <div className="mb-5 pb-3 text-center">
                 <h2 className="text-uppercase">Nice Work!</h2>
                 <p className="fs-5 mb-0">
-                    Thank you for taking meaningful climate action. Research shows that using our own voices and our own
-                    words is the most impactful and effective way to influence our policymakers. Every action counts as
-                    we press our leaders to prioritize immediate and ambitious climate action!
+                    Thank you for taking meaningful climate action. Research shows that using personalized messaging is
+                    the most effective way to influence our policymakers. Together, we can make real change.
                 </p>
                 <div className="d-flex m-auto justify-content-center w-100 mt-3">
                     <div className={styles.buttonWidth}>
                         <Button
                             type="submit"
-                            className="w-100 text-dark"
+                            className="w-100 text-dark p-3"
                             variant="primary"
                             onClick={() => history.push("/pick-your-issue")}
                         >
                             Choose Another Issue
-                        </Button>
-                        <Button className="w-100 mt-2" variant="secondary" onClick={() => window.close()}>
-                            End Session
                         </Button>
                     </div>
                 </div>
