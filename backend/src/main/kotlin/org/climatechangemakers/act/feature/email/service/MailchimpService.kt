adc981081473a259ce3305d7086d1dca0b895d6f
Kevin Cianfarini <kevincianfarini@gmail.com>
2022-02-09 15:15:44 -0500

    Enroll changemakers in our newsletter (#281)
    Closes #34

--- backend/src/main/kotlin/org/climatechangemakers/act/feature/email/service/MailchimpService.kt
+++ backend/src/main/kotlin/org/climatechangemakers/act/feature/email/service/MailchimpService.kt
@@ -1,9 +1,12 @@
 package org.climatechangemakers.act.feature.email.service
 
-import org.climatechangemakers.act.feature.email.model.SubscribeChangemakerRequest
+import org.climatechangemakers.act.feature.email.model.EnrollMemberRequest
+import org.climatechangemakers.act.feature.email.model.SubscribeNewsletterRequest
+import retrofit2.Response
 import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Body
+import retrofit2.http.GET
 
 interface MailchimpService {
 
@@ -11,6 +14,19 @@ interface MailchimpService {
   suspend fun subscribeChangemaker(
     @Path("audience_id") audienceId: String,
     @Path("email_md5_hash_string") emailMd5Hash: String,
-    @Body request: SubscribeChangemakerRequest,
+    @Body request: EnrollMemberRequest,
   )
+
+  @PUT("lists/{audience_id}/members/{email_md5_hash_string}")
+  suspend fun subscribeChangemaker(
+    @Path("audience_id") audienceId: String,
+    @Path("email_md5_hash_string") emailMd5Hash: String,
+    @Body request: SubscribeNewsletterRequest,
+  )
+
+  @GET("lists/{audience_id}/members/{email_md5_hash_string}")
+  suspend fun checkSubscription(
+    @Path("audience_id") audienceId: String,
+    @Path("email_md5_hash_string") emailMd5Hash: String,
+  ): Response<Void>
 }
