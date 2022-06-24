5e62557eaf3dfc78b6abfff4ff5a1380fb351fce
Kevin Cianfarini <kevincianfarini@gmail.com>
2022-06-24 08:47:38 -0400

    Update Members of Congress

---
+++ congress/update.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+
+curl --url https://theunitedstates.io/congress-legislators/legislators-current.json --output legislators-current.json
+curl --url https://theunitedstates.io/congress-legislators/legislators-district-offices.json --output legislators-district-offices.json
+curl --url https://theunitedstates.io/congress-legislators/legislators-historical.json --output legislators-historical.json
+curl --url https://theunitedstates.io/congress-legislators/legislators-social-media.json --output legislators-social-media.json
+curl --url https://soapbox.senate.gov/api/active_offices.json | jq > active-offices.json
