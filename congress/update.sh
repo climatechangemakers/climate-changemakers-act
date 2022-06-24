c02f333d7f25e567b6828bcfd41c8f7aec8c55b8
Kevin Cianfarini <kevincianfarini@gmail.com>
2022-06-24 08:54:49 -0400

    set e

--- congress/update.sh
+++ congress/update.sh
@@ -1,5 +1,9 @@
 !#/bin/bash
 
+set -e
+set -u
+set -o pipefail
+
 curl --url https://theunitedstates.io/congress-legislators/legislators-current.json --output legislators-current.json
 curl --url https://theunitedstates.io/congress-legislators/legislators-district-offices.json --output legislators-district-offices.json
 curl --url https://theunitedstates.io/congress-legislators/legislators-historical.json --output legislators-historical.json
