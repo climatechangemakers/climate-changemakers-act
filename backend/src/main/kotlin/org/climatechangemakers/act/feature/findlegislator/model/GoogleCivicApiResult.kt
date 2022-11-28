31c255edf0115abdf37a2fe3ea165b3551cc590f
Kevin Cianfarini <kevincianfarini@gmail.com>
2022-11-27 19:54:54 -0500

    Decode values of GoogleCivicApiResult to JsonElement (#376)
    Closes #353

--- backend/src/main/kotlin/org/climatechangemakers/act/feature/findlegislator/model/GoogleCivicApiResult.kt
+++ backend/src/main/kotlin/org/climatechangemakers/act/feature/findlegislator/model/GoogleCivicApiResult.kt
@@ -1,11 +1,12 @@
 package org.climatechangemakers.act.feature.findlegislator.model
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
 
 private val openCivicDataRegex = """ocd-division/country:us/state:[a-zA-Z]{2}/cd:(\d+)""".toRegex()
 
 @Serializable class GoogleCivicApiResult(
-  private val divisions: Map<String, Map<String, String>>,
+  private val divisions: Map<String, Map<String, JsonElement>>,
 ) {
   val congressionalDistrict: Short get() = openCivicDataRegex
     .matchEntire(divisions.keys.first())
